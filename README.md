bdd838020dade7bd3f18b1f1f47629261051677c
rifky <muh.rifky.harto@gmail.com>
2024-10-10 13:47:56 +0700

    feat : jobsheet 6

---
+++ README.md
@@ -0,0 +1,1 @@
+Repo untuk pembelajaran pemrograman mobile semester 5
---
+++ week6/belanja/lib/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:basic_layout_flutter/models/item.dart';
+import 'package:flutter/widgets.dart';
+
+class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
